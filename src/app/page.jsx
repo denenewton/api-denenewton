718f918b40e4ea35f975a85df7a4bb46d3111acd
Daniel Santos <denenewton@icloud.com>
2024-11-01 19:27:14 -0300

    trying to play repair to home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,35 +1,35 @@
-import client from "@/lib/client";
+//import client from "@/lib/client";
 import { gql } from "@apollo/client";
 
 const QUERY = gql`
-query {
-  movies {
-    id
-    title
-    vote_average
-    vote_count
-    poster_path
-    popularity
-    release_date
-    budget
-    backdrop_path
-    overview
-    homepage
-    production_companies {
-      name
-      origin_country
-    }
-    production_countries {
-      name
+  query {
+    movies {
+      id
+      title
+      vote_average
+      vote_count
+      poster_path
+      popularity
+      release_date
+      budget
+      backdrop_path
+      overview
+      homepage
+      production_companies {
+        name
+        origin_country
+      }
+      production_countries {
+        name
+      }
     }
   }
-}
 `;
 
 export default async function Home() {
-  const { data, loading, error } = await client.query({ query: QUERY} );
+  //const { data, loading, error } = await client.query({ query: QUERY });
 
-  if (loading) return "Loading....";
+ // if (loading) return "Loading....";
 
   return (
     <>
@@ -41,43 +41,46 @@ export default async function Home() {
           </a>
         </p>
       </div>
-      <ul key={'4234jl4j2fslfjs'} className="w-full bg-slate-200">
-        {data.movies.map((el) => (
-          <li key={el.original_title} className="w-full py-5">
-            <div className="w-96 m-auto bg-white flex justify-between gap-x-5 rounded-md shadow-sm py-2 px-3 ">
-              <div className="w-50 h-40  rounded-full">
-                <img
-                  className="w-[100%] h-[100%] object-cover rounded-full"
-                  src={el.backdrop_path}
-                  alt={el.title}
-                />
-              </div>
-              <div className="w-full flex flex-col justify-center text-gray-500 text-[13px]">
-                <a href={el.homepage}>
-                  {" "}
-                  <h2 className="text-2xl font-normal">{el.title}</h2>
-                </a>
-                <p className="font-extralight">
-                  <span className="text-gray-900">Release Date: </span>{" "}
-                  {el.release_date}
-                </p>
-                <p className="font-extralight">
-                  <span className="text-gray-900">Budget:</span> ${el.budget}
-                </p>
-                <p className="font-extralight">
-                  <span className="text-gray-900">Vote average:</span>{" "}
-                  {el.vote_average}
-                </p>
-                {el.production_countries.map((el) => (
-                  <p key={el.id} className="font-extralight">
-                    {el.name}
+
+      {data && (
+        <ul key={"4234jl4j2fslfjs"} className="w-full bg-slate-200">
+          {data.movies.map((el) => (
+            <li key={el.original_title} className="w-full py-5">
+              <div className="w-96 m-auto bg-white flex justify-between gap-x-5 rounded-md shadow-sm py-2 px-3 ">
+                <div className="w-50 h-40  rounded-full">
+                  <img
+                    className="w-[100%] h-[100%] object-cover rounded-full"
+                    src={el.backdrop_path}
+                    alt={el.title}
+                  />
+                </div>
+                <div className="w-full flex flex-col justify-center text-gray-500 text-[13px]">
+                  <a href={el.homepage}>
+                    {" "}
+                    <h2 className="text-2xl font-normal">{el.title}</h2>
+                  </a>
+                  <p className="font-extralight">
+                    <span className="text-gray-900">Release Date: </span>{" "}
+                    {el.release_date}
                   </p>
-                ))}
+                  <p className="font-extralight">
+                    <span className="text-gray-900">Budget:</span> ${el.budget}
+                  </p>
+                  <p className="font-extralight">
+                    <span className="text-gray-900">Vote average:</span>{" "}
+                    {el.vote_average}
+                  </p>
+                  {el.production_countries.map((el) => (
+                    <p key={el.id} className="font-extralight">
+                      {el.name}
+                    </p>
+                  ))}
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
